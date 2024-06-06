82e40cb7de0f5b7d4416bfd8753273b61f0c0e4e
ktdrzd <137915871+ktdrzd@users.noreply.github.com>
2024-06-07 02:08:00 +0300

    css + icons

--- src/components/SideBar/LogOut/LogOut.jsx
+++ src/components/SideBar/LogOut/LogOut.jsx
@@ -1,14 +1,19 @@
 import css from './LogOut.module.css';
-import Icon from '../../Icon/Icon';
+// import Icon from '../../Icon/Icon';
 // і сюди dispatch
+import sprite from '../../../assets/sprite.svg';
 
 const LogOut = () => {
-//   додати dispatch
-    return (
+  //   додати dispatch
+  return (
     <>
       {/* onClick додати */}
       <button className={css.buttonLogOut} type="button">
-        <Icon id="" width="32" height="32" />
+        <div className={css.iconLogOut}>
+          <svg className={css.logOut} width="32" height="32">
+            <use xlinkHref={`${sprite}#icon-login`}></use>
+          </svg>
+        </div>
         Log Out
       </button>
     </>
