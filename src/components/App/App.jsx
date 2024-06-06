14c28b6fcebc7af8565dc0042685c32bdf279ca7
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-06-06 11:31:29 +0300

    add route, storage, Yup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,19 @@
-function App() {}
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Welcome from '../../pages/WelcomePage/WelcomePage';
+import Auth from '../../pages/AuthPage/AuthPage';
+import Home from '../../pages/HomePage/HomePage';
+
+function App() {
+  return (
+    <Suspense>
+      <Routes>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Suspense>
+  );
+}
 
 export default App;
---
+++ src/redux/storage.js
@@ -0,0 +1,35 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  //   persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import authSlice from './auth/slice';
+
+// const authPersistConfig = {
+//   key: 'authSlice',
+//   storage,
+//   whitelist: ['token'],
+// };
+
+// const persistedAuthReducer = persistReducer(authPersistConfig, authSlice);
+
+export const store = configureStore({
+  reducer: {
+    // auth: persistedAuthReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
