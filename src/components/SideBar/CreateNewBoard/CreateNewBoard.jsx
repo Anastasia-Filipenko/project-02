4eba58f909f8d73578dae81c38c98bcf1bbd7773
anyakobtseva <anya.kobtseva@gmail.com>
2024-06-09 18:01:05 +0200

    new create board

--- src/components/SideBar/CreateNewBoard/CreateNewBoard.jsx
+++ src/components/SideBar/CreateNewBoard/CreateNewBoard.jsx
@@ -1,19 +1,43 @@
-import css from '../CreateNewBoard/CreateNewBoard.module.css';
-import sprite from "../../../assets/sprite.svg";
+
+import css from './CreateNewBoard.module.css';
+// import Icon from "../../Icon/Icon";
+import sprite from '../../assets/sprite.svg';
+import { Modal } from '@mui/material';
+import { useRef, useState } from 'react';
+import { BoardModal } from '../BoardModal/BoardModal';
 
 const CreateNewBoard = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedIcon, setSelectedIcon] = useState(null);
+  const ref = useRef();
+
   return (
     <div className={css.create}>
-      {/* onClick додати */}
       <p className={css.buttonTitle}>Create a new board</p>
-      <button className={css.button} type="button">
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => setModalIsOpen(true)}
+      >
         <div className={css.buttonPlus}>
           <svg className={css.iconPlus} width="20" height="20">
             <use xlinkHref={`${sprite}#icon-plus`}></use>
           </svg>
         </div>
       </button>
-      {/* додати відкриття модального вікна */}
+
+      <Modal
+        open={modalIsOpen}
+        onClose={() => setModalIsOpen(false)}
+        disableAutoFocus={true}
+      >
+        <BoardModal
+          ref={ref}
+          selectedIcon={selectedIcon}
+          setSelectedIcon={val => setSelectedIcon(val)}
+          closeModal={() => setModalIsOpen(false)}
+        />
+      </Modal>
     </div>
   );
 };
