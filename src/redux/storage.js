61180d8bae69baabb1eda6e91ff922c22136bd11
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-06-09 20:50:17 +0300

    Update storage.js

--- src/redux/storage.js
+++ src/redux/storage.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import {
   persistStore,
-  //   persistReducer,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
