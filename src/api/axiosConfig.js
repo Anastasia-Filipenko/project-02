8f53316b4296b551a8a0261717d940744f173309
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-06-10 13:19:01 +0300

    icon + api

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,16 @@
+// import axios from 'axios';
+
+// const baseURL = 'http://localhost:3000';
+
+// export const authInstance = axios.create({
+//   baseURL: `${baseURL}/api/card`,
+// });
+
+// authInstance
+//   .get('/')
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.error('Error fetching user profile:', error);
+//   });
