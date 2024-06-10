00dbc50eefa3cd706d462126f92727eaaa80a4c3
ktdrzd <137915871+ktdrzd@users.noreply.github.com>
2024-06-11 01:23:58 +0300

    logOut

--- src/components/SideBar/LogOut/LogOut.jsx
+++ src/components/SideBar/LogOut/LogOut.jsx
@@ -1,16 +1,14 @@
 import css from './LogOut.module.css';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import sprite from '../../../assets/sprite.svg';
-// logout додати з auth-oper
+import {logOut} from '../../../redux/auth/operations';
 
 const LogOut = () => {
-  // const dispatch = useDispatch();
-  // const onLogOut = () => dispatch(logout());
-  // цей момент не точний
+  const dispatch = useDispatch();
+  const onLogOut = () => dispatch(logOut());
   return (
     <>
-    {/*  onClick={onLogOut} */}
-      <button className={css.buttonLogOut} type="button">
+      <button className={css.buttonLogOut} onClick={onLogOut} type="button">
         <div className={css.iconLogOut}>
           <svg className={css.logOut} width="32" height="32">
             <use xlinkHref={`${sprite}#icon-login`}></use>
