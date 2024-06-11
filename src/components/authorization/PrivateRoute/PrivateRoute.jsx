35851706463d1a7a6500be33c2fa6125462dbb87
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-06-11 19:08:21 +0300

    fix

--- src/components/authorization/PrivateRoute/PrivateRoute.jsx
+++ src/components/authorization/PrivateRoute/PrivateRoute.jsx
@@ -11,5 +11,9 @@ export default function PrivateRoute({
 }) {
   const isLoggedIn = useSelector(selectIsLogged);
   const isRefreshing = useSelector(selectIsRefreshing);
-  return isLoggedIn || isRefreshing ? Component : <Navigate to={redirectTo} />;
+  return !isLoggedIn || !isRefreshing ? (
+    Component
+  ) : (
+    <Navigate to={redirectTo} />
+  );
 }
