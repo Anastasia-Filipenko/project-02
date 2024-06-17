14d6a09f12acae6bd2dbd6db597ba60f50be3dfe
Olga-ampilogova <141365234+Olga-ampilogova@users.noreply.github.com>
2024-06-17 13:06:45 +0200

    Fixing dropdown menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,16 +11,6 @@ import DropdownMenu from './Dropdownmenu/Dropdownmenu';
 
 const Header = ({ toggleSidebar, closeSidebar }) => {
   const selectedTheme = useSelector(selectTheme);
-  //const dispatch = useDispatch();
-  // const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  // const handleSelectChange = event => {
-  //   dispatch(setTheme(event.target.value));
-  //   setIsDropdownOpen(false);
-  // };
-  // const toggleDropdown = () => {
-  //   setIsDropdownOpen(!isDropdownOpen);
-  // };
-
   const handleBurgerMenuClick = event => {
     event.stopPropagation();
     toggleSidebar();
@@ -36,28 +26,6 @@ const Header = ({ toggleSidebar, closeSidebar }) => {
         className={clsx(css.header, css[selectedTheme])}
         onClick={handleHeaderClick}
       >
-        {/* <div className={clsx(css.dropdown, css[selectedTheme])}>
-            <button
-              className={clsx(css.dropbtn, css[selectTheme])}
-              onClick={toggleDropdown}
-            >
-              Theme
-              <span className={clsx(css.arrow, css[selectTheme])}>&#9660;</span>
-            </button>
-            {isDropdownOpen && (
-              <div className={clsx(css.dropdownContent, css[selectTheme])}>
-                <button value="light" onClick={handleSelectChange}>
-                  Light
-                </button>
-                <button value="dark" onClick={handleSelectChange}>
-                  Dark
-                </button>
-                <button value="violet" onClick={handleSelectChange}>
-                  Violet
-                </button>
-              </div>
-            )}
-          </div> */}
         <div
           className={clsx(css.burgerMenu, css[selectedTheme])}
           onClick={handleBurgerMenuClick}
@@ -66,9 +34,7 @@ const Header = ({ toggleSidebar, closeSidebar }) => {
         </div>
 
         <div className={clsx(css.themeSection, css[selectedTheme])}>
-          <DropdownMenu
-            className={clsx(css.themeSection, css[selectedTheme])}
-          />
+          <DropdownMenu />
         </div>
 
         <div className={clsx(css.userSection, css[selectedTheme])}>
