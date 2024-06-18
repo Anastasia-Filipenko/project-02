e5d70147fb6a0cf8bed080960037753f978d5bd4
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-06-18 22:13:43 +0300

    fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,11 @@ import loaderGif from '../../assets/kitty.gif';
 export default function Loader() {
   return (
     <div className={css.loader}>
-      <img className={css.loaderGif} src={loaderGif} alt="Loading..." />
+      <img
+        className={css.loaderGif}
+        src="https://res.cloudinary.com/dyqmk6bfl/image/upload/v1718737840/8tuoc0_1_hzapbp.gif"
+        alt="Loading..."
+      />
     </div>
   );
 }
