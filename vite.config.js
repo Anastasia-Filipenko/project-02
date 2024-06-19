d94486d588af6a2fb926c7bc68e881f707982efa
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-06-19 11:53:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: 'terser', // Використання Terser для мінімізації
-    terserOptions: {
-      compress: {
-        drop_console: true, // Видалення console.log
-        drop_debugger: true, // Видалення debugger
-      },
-      mangle: {
-        properties: {
-          // Налаштування мінімізації властивостей об'єктів, якщо потрібно
-        },
-      },
-      output: {
-        comments: false, // Видалення коментарів
-      },
-    },
-  },
 });
